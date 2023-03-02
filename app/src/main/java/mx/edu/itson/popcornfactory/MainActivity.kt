1ca829edd15f3e25d95e95497bef6b7fd824751f
Alec Demian Santana Celaya <Alec.Santana216750@Potros.itson.edu.mx>
2023-03-02 13:01:39 -0700

    Creation Grid View
    Base Grid View with movie theme

---
+++ app/src/main/java/mx/edu/itson/popcornfactory/MainActivity.kt
@@ -0,0 +1,20 @@
+package mx.edu.itson.popcornfactory
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.botonP) as Button
+
+        button.setOnClickListener {
+            var intento: Intent = Intent(this, Catalogo::class.java)
+            startActivity(intento)
+        }
+    }
+}
---
+++ app/src/main/java/mx/edu/itson/popcornfactory/Pelicula.kt
@@ -0,0 +1,3 @@
+package mx.edu.itson.popcornfactory
+
+class Pelicula (var titlulo:String, var image:Int, var header:Int, var sinopsis:String)
